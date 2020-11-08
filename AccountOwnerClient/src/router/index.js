1e681b1ec65b0e4c7443536204ba2be6aceb4843
rasoolkhan <rasoolkhan@cybermindworks.com>
2020-11-08 22:21:56 +0530

    second commit

--- AccountOwnerClient/src/router/index.js
+++ AccountOwnerClient/src/router/index.js
@@ -1,6 +1,7 @@
+/* eslint-disable import/no-unresolved */
 import Vue from 'vue';
 import Router from 'vue-router';
-import HelloWorld from '@/components/HelloWorld';
+import Home from '@/components/Home';
 
 Vue.use(Router);
 
@@ -8,8 +9,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld,
+      name: 'Home',
+      component: Home,
     },
   ],
 });
